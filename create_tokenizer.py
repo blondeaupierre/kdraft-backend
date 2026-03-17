32275b5f80bb9c772b7997e6204c7a91ad692724
pblon <niono235@gmail.copm>
2026-03-17 09:19:49 +0100

    remove configs

--- create_tokenizer.py
+++ create_tokenizer.py
@@ -2,12 +2,9 @@ import yaml
 
 from src.utils.tokenizer_builder import DraftTokenizerBuilder
 
-with open("config/tokenizer_config.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
 builder = DraftTokenizerBuilder(
-    vocab_dir=config["vocab_dir"],
-    save_dir=config["save_dir"],
+    vocab_dir="resources/vocab",
+    save_dir="resources/tokenizer",
     log_level="INFO"
 )
 builder.save_tokenizer()
